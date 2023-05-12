38c14d79eb38b076a9ffcb092e091cdf977e2b9a
jaydeep <jaydeepk@gmail.com>
2023-05-12 19:59:22 +0530

    Added exception handling for call to products api

--- db/Products.py
+++ db/Products.py
@@ -5,4 +5,8 @@ class Products:
     products_api = 'http://127.0.0.1:8080/products'
 
     def search(self, product_type: str):
-        return requests.get(self.products_api, params={'type': product_type})
+        try:
+            return requests.get(self.products_api, params={'type': product_type})
+        except requests.exceptions.RequestException as e:
+            print(f'An error occurred while connecting to {self.products_api}', e)
+            raise Exception(f'An error occurred while connecting to {self.products_api}' + str(e))
