74c526ffdfaa29cba29cd180a97b2d91d8df434a
jaydeep <jaydeepk@gmail.com>
2023-05-16 16:57:45 +0530

    Updated to Specmatic 0.2.3 Added order api spec.

--- test/test_contract.py
+++ test/test_contract.py
@@ -1,6 +1,6 @@
 import pytest
 
-from specmatic_python.specmatic.decorators import start_flask_app, specmatic_stub, specmatic_contract_test
+from specmatic.core.decorators import start_flask_app, specmatic_stub, specmatic_contract_test
 from api import app
 from definitions import ROOT_DIR
 
@@ -11,12 +11,12 @@ stub_port = 8080
 specmatic_json_file = ROOT_DIR + '/specmatic.json'
 expectation_json_file = ROOT_DIR + '/test/data/expectation.json'
 service_contract_file = ROOT_DIR + '/test/spec/product-search-bff-api.yaml'
-stub_contract_file = ROOT_DIR + '/test/spec/order_api_spec.yaml'
+stub_contract_file = ROOT_DIR + '/test/spec/api_order_v1.yaml'
 
 
-@specmatic_contract_test(host, port, '', service_contract_file)
-@specmatic_stub(stub_host, stub_port, [expectation_json_file], '', stub_contract_file)
+@specmatic_contract_test(host, port, service_contract_file)
 @start_flask_app(app, host, port)
+@specmatic_stub(stub_host, stub_port, [expectation_json_file])
 class TestApiContract:
     @classmethod
     def teardown_class(cls):
